ad4f60afcab47a292378b225019cfcfd81e425dc
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-01-28 08:45:21 -0500

    VariableDemo.java

---
+++ topics/01-basics/VariableDemo.java
@@ -0,0 +1,19 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+      
+      String patientName = "kira";
+      int PatientHeight = 2;
+      int PatientAge = 3;
+      double PatientWeight = 10.5;//used double because it gives greater precison and wider range than 32 bit (float)
+      char patientSex = 'F';
+
+      
+      
+      System.out.println("patientName " + patientName);
+      System.out.println("PatientHeight " + PatientHeight);
+      System.out.println("PatientAge " + PatientAge);
+      System.out.println("PatientWeight " + PatientWeight);
+      System.out.println("patientSex " + patientSex);
+    }
+  }
+  
