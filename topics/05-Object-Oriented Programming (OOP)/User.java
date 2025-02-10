6600d118bb61998073fba57119dd8ccdd7715929
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-02-10 07:38:26 -0500

    init commit

---
+++ topics/05-Object-Oriented Programming (OOP)/User.java
@@ -0,0 +1,21 @@
+
+public class User {
+    private String name;
+    String membership;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setMembership(String membership) {
+        this.membership = membership;
+    }
+
+    public String getMembership() {
+        return this.membership;
+    }
+}
