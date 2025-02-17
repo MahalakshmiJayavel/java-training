dbcd261a20f0fd75324182e01b4c4c03706b7c4d
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-02-17 12:30:21 -0500

    Commit init

---
+++ topics/06-Collections-Framework/LinkedListDemo.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> name = new LinkedList<>();
+        name.add("squid");
+        name.add("tom");
+        name.add("brady");
+        name.add("snoopy");
+        name.add("yor");
+            System.out.println(name);
+        }
+    }
