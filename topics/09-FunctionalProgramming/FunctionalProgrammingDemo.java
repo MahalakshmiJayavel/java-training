a8937aa370dd8f2501f4b473986b49f966061e61
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-05 19:25:36 -0500

    Update FunctionalProgrammingDemo.java

--- topics/09-FunctionalProgramming/FunctionalProgrammingDemo.java
+++ topics/09-FunctionalProgramming/FunctionalProgrammingDemo.java
@@ -1,15 +1,1 @@
-import java.util.List;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-
-public class FunctionalProgrammingDemo {
-
-    public static List<Integer> getCubesOfFirstNNumbers(int n) {
-        return IntStream.range(1,n + 1)  //helps to generate numbers from 1 to n
-                .map(number->number*number*number) //maps number to its cube value
-                .boxed()// convert int stream to stream of intergers
-                .collect(Collectors.toList());
-    }
-}
