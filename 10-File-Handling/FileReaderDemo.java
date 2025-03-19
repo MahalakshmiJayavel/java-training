316c9ef2b38c7303ea1fa3572937e38e36a9638f
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 17:21:05 -0400

    Update FileReaderDemo.java

--- 10-File-Handling/FileReaderDemo.java
+++ 10-File-Handling/FileReaderDemo.java
@@ -1,18 +1,19 @@
-import java.io.FileReader;
+import java.io.FileReader;  
+import java.io.IOException; 
 
 public class FileReaderDemo {
     public static void main(String[] args) {
-        try {
-            FileReader fr = new FileReader("output.txt");
-            int c = fr.read();
-            while (c != -1)
-            {
+        // Using try-with-resources to ensure FileReader is closed automatically
+        try (FileReader fr = new FileReader("output.txt")) {
+            int c;
+
+            // Reading and printing characters until end of file (-1)
+            while ((c = fr.read()) != -1) {
                 System.out.print((char) c);
-                c = fr.read();
             }
-            fr.close();
+        } 
+        catch (IOException e) { // Handling file reading errors
+            System.out.println("Error reading the file: " + e.getMessage());
         }
-        catch (Exception e) {
-     }
-}
+    }
 }
