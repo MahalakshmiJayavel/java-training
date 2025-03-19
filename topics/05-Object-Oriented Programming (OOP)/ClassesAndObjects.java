ae07c8385726ea5b32d59501aa29eb77688dcbf9
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 16:25:51 -0400

    Update ClassesAndObjects.java

--- topics/05-Object-Oriented Programming (OOP)/ClassesAndObjects.java
+++ topics/05-Object-Oriented Programming (OOP)/ClassesAndObjects.java
@@ -1,11 +1,14 @@
 public class ClassesAndObjects {
-    int year = 2023;
-    String brand = "Tesla";
+    // Instance variables (attributes of the object)
+    int year = 2023; // Default manufacturing year
+    String brand = "Tesla"; // Default car brand
 
     public static void main(String[] args) {
+        // Creating an object of the class
         ClassesAndObjects obj = new ClassesAndObjects();
 
-        System.out.println("Car Brand: " + obj.brand);
-        System.out.println("Manufacturing Year: " + obj.year);
+        // Accessing and printing the instance variables using the object
+        System.out.println("Car Brand: " + obj.brand); 
+        System.out.println("Manufacturing Year: " + obj.year); 
     }
 }
