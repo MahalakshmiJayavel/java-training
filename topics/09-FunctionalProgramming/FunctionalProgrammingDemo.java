f4e89b642f179d2af173f19c876110cb1f66b6c0
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 16:49:26 -0400

    Update FunctionalProgrammingDemo.java

--- topics/09-FunctionalProgramming/FunctionalProgrammingDemo.java
+++ topics/09-FunctionalProgramming/FunctionalProgrammingDemo.java
@@ -1,12 +1,14 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.Arrays;  // Importing Arrays utility class
+import java.util.List;     // Importing List interface
 
 public class FunctionalProgrammingDemo {
     public static void main(String[] args) {
+        // Creating a list of numbers using Arrays.asList()
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        numbers.stream()          
-               .filter(n -> n % 2 == 0)  
-               .forEach(System.out::println);  
+        // Using Stream API to filter even numbers and print them
+        numbers.stream()           // Convert list to a Stream
+               .filter(n -> n % 2 == 0)  // Lambda expression to filter even numbers
+               .forEach(System.out::println);  // Print each even number
     }
 }
