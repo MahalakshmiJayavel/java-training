77a3d5f9c01d508a15fbacafd31e08bdded9d226
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 15:42:09 -0400

    Update PolymorphismDemo.java

--- topics/05-Object-Oriented Programming (OOP)/PolymorphismDemo.java
+++ topics/05-Object-Oriented Programming (OOP)/PolymorphismDemo.java
@@ -1,10 +1,23 @@
+// Parent class (superclass)
+class Animal {
+    public void sound() {
+        System.out.println("Animals make sounds");
+    }
+}
+
+// Child class (subclass) overriding sound() method
+class Lion extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Lion roars");
+    }
+}
+
+// Main class to demonstrate polymorphism
 public class PolymorphismDemo {
     public static void main(String[] args) {
-        Lion a = new Lion();
-        a.sound();
-
-
+        Animal a = new Lion(); 
+        a.sound(); // Calls overridden method in Lion class
     }
-
 }
 
