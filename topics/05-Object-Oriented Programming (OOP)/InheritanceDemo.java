17ce260c232aa3e0d9285a6ae2ec57c80a1bbbb1
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 15:51:53 -0400

    Update InheritanceDemo.java

--- topics/05-Object-Oriented Programming (OOP)/InheritanceDemo.java
+++ topics/05-Object-Oriented Programming (OOP)/InheritanceDemo.java
@@ -1,11 +1,26 @@
+// Parent class
+class Animal {
+    String name = "Kitty"; // Default name
+    int age = 2; // Default age
+}
 
+// Child class inheriting from Animal
+class Cat extends Animal {
+    // No additional properties needed since it inherits from Animal
+}
 
+// Main class
 public class InheritanceDemo {
-
     public static void main(String[] args) {
-        Cat a = new Cat();
-        System.out.println(a.name);
+        Cat a = new Cat(); // Cat object inherits name & age from Animal
+        System.out.println(a.name); // Accessing inherited property
         System.out.println(a.age);
-
     }
 }
+
+
+
+
+
+
+
