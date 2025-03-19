c88ceccd6607e43acfa23bddf24bb46521fa510f
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 16:09:36 -0400

    Update ArraysDemo.java

--- topics/04-arrays/ArraysDemo.java
+++ topics/04-arrays/ArraysDemo.java
@@ -1,6 +1,9 @@
 public class ArraysDemo {
     public static void main(String[] args) {
-        String[] names = {"Maha","Shamee","Shwe"};
-            System.out.println(names[0]);
+        // Declaring and initializing a String array with names
+        String[] names = {"Maha", "Shamee", "Shwe"};
+
+        // Printing the first element of the array (index 0)
+        System.out.println("First name in the array: " + names[0]); // Output: Maha
     }
 }
