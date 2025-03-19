51f14ef6ad5470562bb606f52c1af7753b154160
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 16:05:38 -0400

    Update MapDemo.java

--- topics/06-Collections-Framework/MapDemo.java
+++ topics/06-Collections-Framework/MapDemo.java
@@ -1,13 +1,22 @@
-import java.util.HashMap;
+import java.util.HashMap; 
 
 public class MapDemo {
     public static void main(String[] args) {
+        // Creating a HashMap to store String keys and Integer values
         HashMap<String, Integer> info = new HashMap<>();
-        info.put("maha",1);
-        info.put("shamee",2);
-        info.put("shwe",3);
-        System.out.println(info.get("maha"));
-        System.out.println(info.containsValue(2));
-        System.out.println(info);
+
+        // Adding key-value pairs to the HashMap
+        info.put("maha", 1);
+        info.put("shamee", 2);
+        info.put("shwe", 3);
+
+        // Retrieving and printing the value associated with the key "maha"
+        System.out.println("Value associated with 'maha': " + info.get("maha")); 
+
+        // Checking if the HashMap contains the value '2'
+        System.out.println("Does the HashMap contain value 2? " + info.containsValue(2)); 
+
+        // Printing the entire HashMap
+        System.out.println("Complete HashMap: " + info);
     }
 }
