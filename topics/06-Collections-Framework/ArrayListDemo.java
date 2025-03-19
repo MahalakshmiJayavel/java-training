31bf0a4c04b6ba57e50ff99c1ad8d4483dd0553e
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 16:01:38 -0400

    Update ArrayListDemo.java

--- topics/06-Collections-Framework/ArrayListDemo.java
+++ topics/06-Collections-Framework/ArrayListDemo.java
@@ -1,18 +1,23 @@
-import java.util.ArrayList;
+import java.util.ArrayList; 
 
 public class ArrayListDemo {
-        public static void main(String[] args) {
-            ArrayList<Integer> a = new ArrayList<>();
-            a.add(1);
-            a.add(2);
-            a.add(3);
-            a.add(4);
-            a.add(5);
-            a.remove(1);
-            for (int i:a) {
-                System.out.println(i);
-            }
+    public static void main(String[] args) {
+        // Create an ArrayList of Integers
+        ArrayList<Integer> a = new ArrayList<>();
+
+        // Adding elements to the ArrayList
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+
+        // Removing the element at index 1 (which is '2')
+        a.remove(1); // After removal, list becomes [1, 3, 4, 5]
+
+        // Iterating through the ArrayList and printing elements
+        for (int i : a) {
+            System.out.println(i);
         }
     }
-
-
+}
