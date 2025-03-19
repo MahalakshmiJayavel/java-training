cf33a7b1c38e54dd702a5d8414ac866f479cfc38
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 17:34:35 -0400

    Update DateAndTimeAPI.java

--- 11-Java-8+Features/DateAndTimeAPI.java
+++ 11-Java-8+Features/DateAndTimeAPI.java
@@ -1,19 +1,19 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.LocalDateTime;
+import java.time.LocalDate;     
+import java.time.LocalTime;     
+import java.time.LocalDateTime; 
 
 public class DateAndTimeAPI {
     public static void main(String[] args) {
-        LocalDate currentDate= LocalDate.now();
-        System.out.println("currentDate: "+ currentDate);
+        // Getting the current system date
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Current Date: " + currentDate); 
 
-        LocalTime currentTime= LocalTime.now();
-        System.out.println("currentTime: "+ currentTime);
+        // Getting the current system time
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current Time: " + currentTime); 
 
-        LocalDateTime currentDateTime= LocalDateTime.now();
-        System.out.println("currentDateTime: "+ currentDateTime);
-
-
+        // Getting the current system date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("Current Date and Time: " + currentDateTime); 
     }
-
 }
