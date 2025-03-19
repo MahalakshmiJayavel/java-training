ba9dae8c2ad4748c4d9caf79fc1f0d7ce13f319b
MahalakshmiJayavel <mahalakshmijeyavel@gmail.com>
2025-03-19 16:17:30 -0400

    Update IfElseDemo.java

--- topics/03-control-structures/IfElseDemo.java
+++ topics/03-control-structures/IfElseDemo.java
@@ -1,10 +1,12 @@
 public class IfElseDemo {
     public static void main(String[] args) {
-        int a = 50;
+        int a = 50; // Input: score or value to check
+
+        // Checking if 'a' is less than or equal to 40
         if (a <= 40) {
-            System.out.println("fail");
+            System.out.println("Fail"); 
         } else {
-            System.out.println("pass");
+            System.out.println("Pass"); 
         }
     }
 }
